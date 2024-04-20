4bfacbe703841a06b00f5760ef6ad65752f3d17e
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 09:19:33 +0530

    Understanding data types

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+let x = 5;// number
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(typeof bigInt);
+
+let name = 'John';// string
+
+let isTrue = true; //boolean
+
+let a = null; //null
+
+var age; //undefined
+console.log(age);
+
+let person = {
+    name: 'John',
+    age: 25,
+}
+
+let id = Symbol('id');
