0cbd6d83975a07eccf6be5561db92da71bb44d82
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 11:50:11 +0530

    Conditional statements (if/else, switch)

---
+++ Conditional.js
@@ -0,0 +1,34 @@
+let age = 18;
+if(age >= 18){
+    console.log('You are eligible to vote');
+}else{
+    console.log('You are not eligible to vote');
+}
+
+let day = 2;
+switch(day){
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;
+    case 7:
+        console.log('Sunday');
+        break;
+    default:
+        console.log('Invalid day');
+        break;
+}
