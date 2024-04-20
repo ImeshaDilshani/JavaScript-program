1c67b36373c5d5dca79b0535a8edd32b09538e01
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 08:38:54 +0530

    Understanding variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+let name;
+name = 'John';
+console.log(name);
+
+// const PI = 3.14159;
+// PI = 4;
+// console.log(PI); 
+
+var count = 0;
+if(true){
+    var count = 1;
+    console.log(count);
+}
+console.log(count);
+
