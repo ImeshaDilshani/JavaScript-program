ef64dc2f2dac02f03fa19db8493e5d9246f7e407
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 09:42:10 +0530

    Using arithmetic and comparison operators

---
+++ operators.js
@@ -0,0 +1,32 @@
+let x = 10;
+let y = 5;
+
+let sum = x + y;
+
+console.log(sum);
+
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
+let results = x ** y;
+
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+console.log(results);
+
+//comparison operators
+
+let a = 5;
+let b = 10;
+
+console.log(a == b);
+console.log(a != b);
+
+console.log(a === b);
+console.log(a !== b);
+
+console.log(a > b);
+console.log(a != b);
