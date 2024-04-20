0f4aa68a3b871b3651ed811667b12a782cad4e37
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 11:02:56 +0530

    Working with strings

---
+++ arrays.js
@@ -0,0 +1,10 @@
+//fruits
+
+let fruits = ['apple', 'banana', 'mango', 'orange', 'papaya'];
+console.log(fruits[0]);
+
+fruits.push('grapes');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
---
+++ strings.js
@@ -0,0 +1,9 @@
+let str1 = "hello world";
+let str2 = 'This is a String';
+let str3 = `12345`;
+
+console.log(typeof str1, typeof str2, typeof str3);
+
+console.log(`1+2 = ${1+2}`);
+
+console.log(str1.length);
