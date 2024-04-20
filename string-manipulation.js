474cd109a0483278e68cb5ca078a1d5509affc0c
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-20 11:34:29 +0530

    string manipulation

---
+++ string-manipulation.js
@@ -0,0 +1,14 @@
+let str1 = 'hello';
+let str2 = 'world';
+let results = str1 + ' ' + str2;
+
+let resultTwo = str1.concat(' ', str2);
+
+console.log(results);
+console.log(resultTwo);
+
+let str = "Hello, World!";
+let result = str.slice(7, 12);
+console.log(result);
+let resultThree = str.substring(7,12);
+console.log(result);
